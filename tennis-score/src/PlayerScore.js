cd58d15810ab4ee58769583af00feb4c6ccc778b
Thomas Le Vaou <thomas.levaou@protonmail.com>
2023-08-15 18:06:31 +0200

    solution for player score exhcange compute

--- tennis-score/src/PlayerScore.js
+++ tennis-score/src/PlayerScore.js
@@ -1,14 +1,16 @@
 import { useSelector } from 'react-redux'
-import { selectPlayerHasAdvantage } from './selectors'
+import { selectPlayerHasAdvantage, countHitsLeftUntilVictory } from './selectors'
 
 export function PlayerScore ({ playerId, playerName }) {
     const score = useSelector((state) => state[playerId]);
     const hasAdvantage = useSelector(selectPlayerHasAdvantage(playerId));
+    const hitsLeftUntilVictory = useSelector(countHitsLeftUntilVictory(playerId));
 
     return (
         <div className="player-score">
             <p>{playerName}</p>
             <p>{(hasAdvantage ? "Avantage - " : "") + score}</p>
+            {hitsLeftUntilVictory !== 0 && (<p>À {hitsLeftUntilVictory} échange(s) de la victoire</p>)}
         </div>
     );
 }
--- tennis-score/src/selectors.js
+++ tennis-score/src/selectors.js
@@ -4,7 +4,33 @@ export const selectPlayerHasAdvantage = (playerId) => {
 
 export const countWonGamesOfPlayer = (playerId) => {
     return (state) => {
-        let gamesWithPlayerIdAsWinner = state.history.filter((game) => game.winner === playerId);
+        const gamesWithPlayerIdAsWinner = state.history.filter((game) => game.winner === playerId);
         return gamesWithPlayerIdAsWinner.length;
     };
+}
+
+export const countHitsLeftUntilVictory = (playerId) => {
+    return (state) => {
+        if (state.winner) {
+            return 0;
+        }
+        const otherPlayer = playerId === "player1" ? "player2" : "player1";
+        const currentPlayerScore = state[playerId];
+        const otherPlayerScore = state[otherPlayer];
+        const necessaryHitsToWinIfOpponentDidNotReach40 = {
+            0: 4,
+            15: 3,
+            30: 2,
+            40: 1
+        }
+        if (otherPlayerScore === 40) {
+            if (state.advantage === null) {
+                return necessaryHitsToWinIfOpponentDidNotReach40[currentPlayerScore] + 1;
+            }
+            if (state.advantage === otherPlayer) {
+                return necessaryHitsToWinIfOpponentDidNotReach40[currentPlayerScore] + 2;
+            }
+        }
+        return necessaryHitsToWinIfOpponentDidNotReach40[currentPlayerScore];
+    }
 }
