478ab219b4ff1b4491210666088625cd087dd8a5
Thomas Le Vaou <thomas.levaou@protonmail.com>
2023-08-15 15:42:59 +0200

    My version of the exercise

--- tennis-score/src/Display.js
+++ tennis-score/src/Display.js
@@ -5,6 +5,27 @@ export function Display() {
   const gameIsPlaying = useSelector((state) => state.playing);
   const player1Score = useSelector((state) => state.player1);
   const player2Score = useSelector((state) => state.player2);
+  const gameWinner = useSelector((state) => state.winner);
+  const advantage = useSelector((state) => state.advantage);
 
-  return <p class="display">{ gameIsPlaying ? "Le score est " + player1Score + " - " + player2Score : "C'est la pause"  }</p>;
+  const updateScoreText = () => {
+    if (gameWinner) {
+      let gameWinnerLabel = gameWinner === "player1" ? '1': '2';
+      return "Joueur " + gameWinnerLabel + " gagne";
+    } else if (gameIsPlaying) {
+      let scoreLabel = "Le score est " + player1Score + " - " + player2Score;
+      if (advantage) {
+        if (advantage === "player1") {
+          scoreLabel += " avantage joueur 1";
+        } else {
+          scoreLabel += " avantage joueur 2";
+        }
+      }
+      return scoreLabel;
+    } else {
+      return "C'est la pause";
+    }
+  };
+
+  return <p class="display">{ updateScoreText() }</p>;
 }
