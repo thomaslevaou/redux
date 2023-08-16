100a751c4d924269dc8cf2bbd87a14d26a6da7be
Thomas Le Vaou <thomas.levaou@protonmail.com>
2023-08-16 22:26:57 +0200

    adding immer and freelancesReducer

--- shiny-react/src/features/freelances.js
+++ shiny-react/src/features/freelances.js
@@ -1,3 +1,5 @@
+import produce from 'immer'
+
 const initialState = {
   status: 'void',
   data: null,
@@ -11,3 +13,45 @@ const REJECTED = 'freelances/rejected'
 const freelancesFetching = () => ({ type: FETCHING })
 const freelancesResolved = (data) => ({ type: RESOLVED, payload: data })
 const freelancesRejected = (error) => ({ type: REJECTED, payload: error })
+
+export default function freelancesReducer(state = initialState, action) {
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case FETCHING: {
+        if (draft.status === 'void') {
+          draft.status = 'pending'
+          return
+        }
+        if (draft.status === 'rejected') {
+          draft.error = null
+          draft.status = 'pending'
+          return
+        }
+        if (draft.status === 'resolved') {
+          draft.status = 'updating'
+          return
+        }
+        return
+      }
+      case RESOLVED: {
+        if (draft.status === 'pending' || draft.status === 'updating') {
+          draft.data = action.payload
+          draft.status = 'resolved'
+          return
+        }
+        return
+      }
+      case REJECTED: {
+        if (draft.status === 'pending' || draft.status === 'updating') {
+          draft.error = action.payload
+          draft.data = null
+          draft.status = 'rejected'
+          return
+        }
+        return
+      }
+      default:
+        return
+    }
+  })
+}
