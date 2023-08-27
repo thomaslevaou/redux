e0a588d76cce6fb2ac46c20ba5d6afa7fe59d506
Thomas Le Vaou <thomas.levaou@protonmail.com>
2023-08-27 18:15:27 +0200

    removing useless codes in tests

--- Partie_4_optimisation_state_management/4_tests.md
+++ Partie_4_optimisation_state_management/4_tests.md
@@ -1,2 +1,3 @@
 # Mettez votre logique Redux à l’épreuve avec les tests
 
+Si je lance un `npm run test` dans Shiny React comme ça, on peut voir que les tests plantent avec un message de la forme `could not find react-redux context value; please ensure the component is wrapped in a <Provider>`.
--- shiny-react/src/utils/test/index.jsx
+++ shiny-react/src/utils/test/index.jsx
@@ -1,16 +1,9 @@
 import { render as rtlRender } from '@testing-library/react'
-import { ThemeProvider, SurveyProvider } from '../../utils/context'
 import { MemoryRouter } from 'react-router-dom'
 
 export function render(ui, options) {
   function Wrapper({ children }) {
-    return (
-      <MemoryRouter {...options}>
-        <ThemeProvider>
-          <SurveyProvider>{children}</SurveyProvider>
-        </ThemeProvider>
-      </MemoryRouter>
-    )
+    return <MemoryRouter {...options}>{children}</MemoryRouter>
   }
   rtlRender(ui, { wrapper: Wrapper })
 }
