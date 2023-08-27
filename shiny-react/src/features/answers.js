58dcc3452561176d1c3cbd38b277697ae385bc87
Thomas Le Vaou <thomas.levaou@protonmail.com>
2023-08-27 18:42:48 +0200

    removng console.log

--- shiny-react/src/features/answers.js
+++ shiny-react/src/features/answers.js
@@ -5,11 +5,9 @@ const { actions, reducer } = createSlice({
   initialState: {},
   reducers: {
     save: (draft, action) => {
-      console.log(action.payload)
       // il vaut mieux éviter de passer par une déstructuration de draft ici (genre les ...draft), ça bugue sans que je sache pourquoi
       // Mais ici, on retrouve bien la bonne structure du useContext quand même en ne faisant que les modifications dans `Survey`, donc ça va
       draft[action.payload.questionNumber] = action.payload.answer
-      console.log(draft)
     },
   },
 })
