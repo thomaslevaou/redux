b79294374d06001b739b8b4f5c5fd7b6a16c7e1c
Thomas Le Vaou <thomas.levaou@protonmail.com>
2023-08-27 17:12:54 +0200

    Adding createSlice to results

--- shiny-react/src/features/results.js
+++ shiny-react/src/features/results.js
@@ -1,4 +1,4 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 import { selectResults } from '../utils/selectors'
 
 const initialState = {
@@ -8,66 +8,76 @@ const initialState = {
   params: null, // on ne met à jour la page que si les paramètres ont changé, pour la perf
 }
 
-const resultsFetching = createAction('results/fetching', (params) => ({
-  payload: { params },
-}))
-const resultsResolved = createAction('results/resolved', (params, data) => ({
-  payload: { params, data },
-}))
-const resultsRejected = createAction('results/rejected', (params, error) => ({
-  payload: { params, error },
-}))
+const { actions, reducer } = createSlice({
+  name: 'results',
+  initialState,
+  reducers: {
+    fetching: {
+      prepare: (params) => ({
+        payload: { params },
+      }),
+      reducer: (draft, action) => {
+        const params = action.payload.params
+        if (draft.status === 'void') {
+          draft.status = 'pending'
+          draft.params = params
+          return
+        }
+        draft.status = 'updating'
+        draft.params = params
+      },
+      resolved: {
+        prepare: (params, data) => ({
+          payload: { params, data },
+        }),
+        reducer: (draft, action) => {
+          if (draft.params !== action.payload.params) {
+            return
+          }
+          if (draft.status === 'pending' || draft.status === 'updating') {
+            draft.data = action.payload.data
+            draft.status = 'resolved'
+            return
+          }
+          return
+        },
+      },
+      rejected: {
+        prepare: (params, error) => ({
+          payload: { params, error },
+        }),
+        reducer: (draft, action) => {
+          if (draft.params !== action.payload.params) {
+            return
+          }
+          if (draft.status === 'pending' || draft.status === 'updating') {
+            draft.error = action.payload.error
+            draft.data = null
+            draft.status = 'rejected'
+            return
+          }
+        },
+      },
+    },
+  },
+})
 
 export function fetchOrUpdateResults(resultsQueryParams) {
   return async (dispatch, getState) => {
     const results = selectResults(getState())
     if (results.status === 'void' || results.params !== resultsQueryParams) {
-      dispatch(resultsFetching(resultsQueryParams))
+      dispatch(actions.fetching(resultsQueryParams))
       try {
         const response = await fetch(
           `http://localhost:8000/results?${resultsQueryParams}`
         )
         const data = await response.json()
-        dispatch(resultsResolved(resultsQueryParams, data))
+        dispatch(actions.resolved(resultsQueryParams, data))
       } catch (error) {
-        dispatch(resultsRejected(resultsQueryParams, error))
+        dispatch(actions.rejected(resultsQueryParams, error))
       }
     }
   }
 }
 
-export default createReducer(initialState, (builder) =>
-  builder
-    .addCase(resultsFetching, (draft, action) => {
-      const params = action.payload.params
-      if (draft.status === 'void') {
-        draft.status = 'pending'
-        draft.params = params
-        return
-      }
-      draft.status = 'updating'
-      draft.params = params
-    })
-    .addCase(resultsResolved, (draft, action) => {
-      if (draft.params !== action.payload.params) {
-        return
-      }
-      if (draft.status === 'pending' || draft.status === 'updating') {
-        draft.data = action.payload.data
-        draft.status = 'resolved'
-        return
-      }
-      return
-    })
-    .addCase(resultsRejected, (draft, action) => {
-      if (draft.params !== action.payload.params) {
-        return
-      }
-      if (draft.status === 'pending' || draft.status === 'updating') {
-        draft.error = action.payload.error
-        draft.data = null
-        draft.status = 'rejected'
-        return
-      }
-    })
-)
+export default reducer
